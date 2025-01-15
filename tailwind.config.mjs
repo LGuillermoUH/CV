755342ffecdc9446c4cf833bae63d18ab5a120a7
LGuillermoUH <luisg1.luis@gmail.com>
2025-01-14 20:02:35 -0600

    temas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // Usaremos la clase 'dark' para cambiar el tema
 	theme: {
+	  extend: {
 		colors: {
-			't-color-1': "#181C14",
-			't-color-2': "#3C3D37",
-			't-color-3': "#697565",
-			't-color-4': "#ECDFCC",
-			't-text-h1': "#F5E9D8",
-			't-text-h2': "#B0B3B8",
-			't-text-h3': "#6C6F74"
+		  primary: 'var(--color-primary)',
+		  secondary: 'var(--color-secondary)',
+		  accent: 'var(--color-accent)',
+		  background: 'var(--color-background)',
+		  surface: 'var(--color-surface)',
+		  'text-title': 'var(--color-text-title)',
+		  'text-subtitle': 'var(--color-text-subtitle)',
+		  'text-body': 'var(--color-text-body)',
 		},
-		extend: {},
+	  },
 	},
 	plugins: [
-		require('@tailwindcss/typography'),
+	  require('@tailwindcss/typography'),
 	],
-}
+  }
+  
